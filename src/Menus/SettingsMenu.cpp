ed2a2cc20c398e75b096791d09b412fbc5208705
PineappleJohn <forddarwin6@gmail.com>
2026-03-26 13:07:22 -0500

    Began versioning system

---
+++ src/Menus/SettingsMenu.cpp
@@ -0,0 +1,81 @@
+#include <Menus/menu.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+class SettingsMenu : public Menu {
+    const char* colors[5] = {"Check for updates", "", "", "", "Back"};
+    int16_t selectedValue = 0;
+    bool pressed = false;
+
+    void render(TFT_eSPI *tft)
+    {
+        try
+        {
+            tft->setTextColor(TFT_WHITE, TFT_BLACK);
+            tft->fillScreen(TFT_BLACK);
+            tft->setTextFont(2);
+            tft->drawString("Settings", 130, 2);
+
+            tft->drawLine(15, 25, 305, 25, TFT_WHITE);
+
+            if (selectedValue >= 5)
+            {
+                selectedValue = 0;
+            }
+            else if (selectedValue < 0)
+            {
+                selectedValue = 5;
+            }
+
+            for (size_t i = 0; i < 5; i++)
+            {
+                if (i == selectedValue)
+                    tft->setTextColor(TFT_BLACK, TFT_WHITE);
+                else
+                {
+                    tft->setTextColor(TFT_WHITE, TFT_BLACK);
+                }
+                tft->drawString(colors[i], 15, 40 + (15 * i));
+            }
+        }
+        catch (...)
+        {
+            printf("Error at rendering");
+        }
+    }
+
+    void start(TFT_eSPI *tft) override
+    {
+        render(tft);
+    }
+
+    int update(TFT_eSPI *tft) override
+    {
+        uint16_t x, y;
+
+        if (tft->getTouch(&x, &y)) {
+            pressed = true;
+            //printf("Y: %d\n", (abs(x - 320) - 60) / 15);
+            selectedValue = (abs(x - 320) - 60) / 20;
+            render(tft);
+        } else if (!tft->getTouch(&x, &y) && pressed) {
+            //printf("SV: %d", selectedValue);
+            pressed = false;
+            switch (selectedValue) {
+                case 0:
+                
+                return -1;
+                case 4:
+                return 1;
+            }
+
+            
+        }
+
+        return -1;
+    }
+
+    int onEvent(TFT_eSPI *tft, Event event) override {
+        return -1;
+    }
+};
--- src/Utils/Constants.h
+++ src/Utils/Constants.h
@@ -3,9 +3,11 @@
 #include <TFT_eSPI.h>
 #include <Menus/menu.h>
 
-#define ENCODER_VCC = 0;
-#define ENCODER_SW  = 0;
-#define ENCODER_A   = 0;
-#define ENCODER_B   = 0;
+#define ENCODER_VCC  0
+#define ENCODER_SW   0
+#define ENCODER_A    0
+#define ENCODER_B    0
+
+#define UPDATE_URL ""
 
 //#endif.
