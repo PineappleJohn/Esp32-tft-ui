e1257a3f1c3cceae3322093a71e417ef26cd95e3
PineappleJohn <forddarwin6@gmail.com>
2026-03-27 10:24:30 -0500

    Fixed for building

--- src/Utils/Constants.h
+++ src/Utils/Constants.h
@@ -1,7 +1,5 @@
 //#ifndef CONSTANTS
 //#define CONSTANTS
-#include <TFT_eSPI.h>
-#include <Menus/menu.h>
 
 #define ENCODER_VCC  0
 #define ENCODER_SW   0
@@ -10,6 +8,6 @@
 
 #define UPDATE_URL "https://github.com/PineappleJohn/Esp32-tft-ui/raw/refs/heads/main/Public-version.txt"
 
-extern bool debugMode = false;
+static bool debugMode = false;
 
 //#endif.
